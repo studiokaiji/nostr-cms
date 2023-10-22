cce97f7a34fd7754024f6efcaf901b2146e3e297
studiokaiji <studiokaiji@gmail.com>
2023-10-23 07:40:14 +0900

    ページネーションを実装

--- src/pages/contents/[schemaId]/index.tsx
+++ src/pages/contents/[schemaId]/index.tsx
@@ -3,16 +3,21 @@ import {
   ContentsTarget,
   ContentsTargetSelect,
 } from "@/components/ContentsTargetSelect";
-import { getContents, getContentsSchema } from "@/services/content";
-import { Button, Flex, Stack, Title } from "@mantine/core";
+import { Content, getContents, getContentsSchema } from "@/services/content";
+import { Schema } from "@/services/general/schema";
+import { ActionIcon, Button, Flex, Stack, Title } from "@mantine/core";
+import { IconChevronLeft, IconChevronRight } from "@tabler/icons-react";
 import { readyNostr } from "nip07-awaiter";
 import { useState } from "react";
 import { useTranslation } from "react-i18next";
 import { useParams } from "react-router-dom";
 import useSWR from "swr";
+import useSWRInfinite from "swr/infinite";
 
 const nostr = await readyNostr;
 const pubkey = await nostr.getPublicKey();
+
+const LIMIT = 30;
 
 export const ContentSchemaPage = () => {
   const { schemaId } = useParams();
@@ -26,27 +31,51 @@ export const ContentSchemaPage = () => {
 
   const [articlesTarget, setArticlesTarget] = useState<ContentsTarget>("all");
 
-  const { data: contents } = useSWR(
-    schema ? [`contents/${schemaId}`, articlesTarget] : null,
+  const getKey = (
+    _: number,
+    previousData: Content[] | null
+  ): [string, Schema, ContentsTarget, Content[] | null, number] | null => {
+    if (!schema || (previousData && !previousData.length)) return null;
+    return [
+      `contents/${schemaId}`,
+      schema,
+      articlesTarget,
+      previousData,
+      LIMIT,
+    ];
+  };
+
+  const {
+    data: contents,
+    isLoading,
+    size,
+    setSize,
+  } = useSWRInfinite(
+    getKey,
     // eslint-disable-next-line @typescript-eslint/no-unused-vars
-    ([_url, target]) =>
+    ([_url, schema, target, prev, limit]) =>
       getContents(
         { target },
         {
           authors:
-            schema!.writeRule.rule === "allowList"
+            schema.writeRule.rule === "allowList"
               ? schema?.writeRule.pubkeys
               : schema!.writeRule.rule === "onlyAuthor"
               ? [pubkey]
               : undefined,
           "#s": schemaId === "articles" ? undefined : [schemaId],
+          limit,
+          until:
+            prev && prev.length
+              ? prev.slice(-1)[0].event.created_at
+              : undefined,
         }
       )
   );
 
   const { t } = useTranslation();
 
-  if (!schema) {
+  if (!schema || !contents) {
     return <div />;
   }
 
@@ -62,7 +91,23 @@ export const ContentSchemaPage = () => {
           + {t("contents.addContent")}
         </Button>
       </Flex>
-      <ContentsTable schema={schema} contents={contents} />
+      <ContentsTable schema={schema} contents={contents[size - 1]} />
+      <Flex gap="xs">
+        <ActionIcon
+          variant="light"
+          disabled={size <= 1 || isLoading}
+          onClick={() => setSize((size) => size - 1)}
+        >
+          <IconChevronLeft />
+        </ActionIcon>
+        <ActionIcon
+          variant="light"
+          disabled={contents[size - 1]?.length < LIMIT || isLoading}
+          onClick={() => setSize((size) => size + 1)}
+        >
+          <IconChevronRight />
+        </ActionIcon>
+      </Flex>
     </Stack>
   );
 };
